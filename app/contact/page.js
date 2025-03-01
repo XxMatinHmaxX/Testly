37f67d89e699d14a1885c74a1de73335432a0f9c
XxMatinHmaxX <matinhma111@gmail.com>
2025-03-01 20:16:37 +0330

    Updated project to new version

--- app/contact/page.js
+++ app/contact/page.js
@@ -21,10 +21,10 @@ export default function Example() {
     <header className="bg-white">
       <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img alt="" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
